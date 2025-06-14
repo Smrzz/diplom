c9cb34deec5cf4540eb1d59c31a65bd8cd6c058b
Smertz <ivan.kishkin2004@yandex.com>
2025-06-15 00:30:59 +0500

    ku

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='home'),  # Главная страница
+    path('', views.index, name='home'),  
     path('ruk', views.ruk_view, name='ruk'),   
     path('safe', views.safe_view, name='safe'), 
     path('turism', views.turism_view, name='turism'), 
